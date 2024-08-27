ba604582feec94557de5dbd314fdf7b8a997d04a
KI JONG BYUN <kjbyun0@gmail.com>
2024-08-27 11:40:42 -0500

    Add a solutioin

--- javascript/selection_sort_recursive.js
+++ javascript/selection_sort_recursive.js
@@ -1,5 +1,15 @@
+
 function selectionSortRecursive(arr) {
   // type your code here
+
+  if (arr.length <= 1)
+    return arr;
+
+  const min = Math.min(...arr);
+  const idx = arr.indexOf(min);
+  arr.splice(idx, 1);
+
+  return [min, ...selectionSortRecursive(arr)];
 }
 
 if (require.main === module) {
@@ -8,6 +18,12 @@ if (require.main === module) {
   console.log("=>", selectionSortRecursive([3, -1, 5, 2]));
 
   console.log("");
+  console.log("Expected: [1]");
+  console.log("=>", selectionSortRecursive([1]));
+
+  console.log("");
+  console.log("Expected: [1,2,3]");
+  console.log("=>", selectionSortRecursive([3,1,2]));
 }
 
 module.exports = selectionSortRecursive;
